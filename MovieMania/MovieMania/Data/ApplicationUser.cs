9ba533d65e7cd51fbedbb20a62bfa84dae930483
Vidhi <vidhi.p.ivorytechnolab@gmail.com>
2025-01-10 17:38:15 +0530

    Added Register and Login functinality

--- MovieMania/MovieMania/Data/ApplicationUser.cs
+++ MovieMania/MovieMania/Data/ApplicationUser.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieMania.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Role { get; set; } 
+        public bool IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedDateTime { get; set; }
     }
-
 }
